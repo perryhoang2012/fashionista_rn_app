ae7b6167273a979bd0f2243132f6ec721d7ac78b
Hoang Huynh <hoanghuynh5555@gmail.com>
2024-09-04 10:06:37 +0700

    feat: done set up bottom tab

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -1,12 +1,65 @@
+import {verticalScale} from '@helpers/uiHelper';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import CartScreen from '@screens/CartScreen';
+import CategoriesScreen from '@screens/CategoriesScreen';
 import HomeScreen from '@screens/HomeScreen';
-import {icon_home, icon_home_active} from '@themes/iconSvg';
+import ProfileScreen from '@screens/ProfileScreen';
+import WishlistScreen from '@screens/WishlistScreen';
+import {
+  icon_cart,
+  icon_cart_active,
+  icon_categories,
+  icon_categories_active,
+  icon_heart,
+  icon_heart_active,
+  icon_home,
+  icon_home_active,
+  icon_profile,
+  icon_profile_active,
+} from '@themes/iconSvg';
 import React from 'react';
 import {SvgXml} from 'react-native-svg';
 
 const HomeIcon = ({focused}: {focused: boolean}) => (
-  <SvgXml xml={focused ? icon_home_active : icon_home} />
+  <SvgXml
+    xml={focused ? icon_home_active : icon_home}
+    width={focused ? 26 : 24}
+    height={focused ? 26 : 24}
+  />
 );
+
+const WishlistIcon = ({focused}: {focused: boolean}) => (
+  <SvgXml
+    xml={focused ? icon_heart_active : icon_heart}
+    width={focused ? 26 : 24}
+    height={focused ? 26 : 24}
+  />
+);
+
+const CategoriesIcon = ({focused}: {focused: boolean}) => (
+  <SvgXml
+    xml={focused ? icon_categories_active : icon_categories}
+    width={focused ? 26 : 24}
+    height={focused ? 26 : 24}
+  />
+);
+
+const CartIcon = ({focused}: {focused: boolean}) => (
+  <SvgXml
+    xml={focused ? icon_cart_active : icon_cart}
+    width={focused ? 26 : 24}
+    height={focused ? 26 : 24}
+  />
+);
+
+const ProfileIcon = ({focused}: {focused: boolean}) => (
+  <SvgXml
+    xml={focused ? icon_profile_active : icon_profile}
+    width={focused ? 26 : 24}
+    height={focused ? 26 : 24}
+  />
+);
+
 const BottomTabNavigator = () => {
   const BottomTab = createBottomTabNavigator();
 
@@ -17,6 +70,7 @@ const BottomTabNavigator = () => {
         tabBarLabel: () => {
           return null;
         },
+        tabBarStyle: {height: verticalScale(84)},
         headerShown: false,
       }}>
       <BottomTab.Screen
@@ -24,6 +78,37 @@ const BottomTabNavigator = () => {
         component={HomeScreen}
         options={{
           tabBarIcon: HomeIcon,
+        }}
+      />
+      <BottomTab.Screen
+        name="Wishlist"
+        component={WishlistScreen}
+        options={{
+          tabBarIcon: WishlistIcon,
+        }}
+      />
+
+      <BottomTab.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          tabBarIcon: CategoriesIcon,
+        }}
+      />
+
+      <BottomTab.Screen
+        name="Cart"
+        component={CartScreen}
+        options={{
+          tabBarIcon: CartIcon,
+        }}
+      />
+
+      <BottomTab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ProfileIcon,
         }}
       />
     </BottomTab.Navigator>
--- src/themes/iconSvg.ts
+++ src/themes/iconSvg.ts
@@ -8,7 +8,26 @@ export const icon_eye_slash = `<svg width="17" height="17" viewBox="0 0 17 17" f
 </svg>
 `;
 
-export const icon_home_active = `<svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+export const icon_home_active = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect opacity="0.01" width="24" height="24" fill="white"/>
+<g clip-path="url(#clip0_2_3444)">
+<g clip-path="url(#clip1_2_3444)">
+<path d="M2.20898 20.7021V9.23072L12.0581 2.30362L21.9072 9.23072V20.7021C21.9072 21.2616 21.3726 21.8823 20.4963 21.8823H3.6199C2.74362 21.8823 2.20898 21.2616 2.20898 20.7021Z" stroke="#004CFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M8.85693 20.925V10.917H15.2587V20.925" stroke="#004CFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+</g>
+<defs>
+<clipPath id="clip0_2_3444">
+<rect width="21.6982" height="21.8012" fill="white" transform="translate(1.20898 1.08105)"/>
+</clipPath>
+<clipPath id="clip1_2_3444">
+<rect width="21.6982" height="21.8012" fill="white" transform="translate(1.20898 1.08105)"/>
+</clipPath>
+</defs>
+</svg>
+`;
+
+export const icon_home = `<svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_0_13358)">
 <rect opacity="0.01" y="0.492188" width="24" height="24" fill="white"/>
 <g clip-path="url(#clip1_0_13358)">
@@ -32,26 +51,97 @@ export const icon_home_active = `<svg width="24" height="25" viewBox="0 0 24 25"
 </svg>
 `;
 
-export const icon_home = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect opacity="0.01" width="24" height="24" fill="white"/>
-<g clip-path="url(#clip0_2_3444)">
-<g clip-path="url(#clip1_2_3444)">
-<path d="M2.20898 20.7021V9.23072L12.0581 2.30362L21.9072 9.23072V20.7021C21.9072 21.2616 21.3726 21.8823 20.4963 21.8823H3.6199C2.74362 21.8823 2.20898 21.2616 2.20898 20.7021Z" stroke="#004CFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M8.85693 20.925V10.917H15.2587V20.925" stroke="#004CFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+export const icon_arrow_right = `<svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M8.67148 0.294922L7.49182 1.49866L11.2475 5.14597H0.0888672V6.83119H11.2475L7.49182 10.4785L8.67148 11.6822L14.5457 5.98858L8.67148 0.294922Z" fill="white"/>
+</svg>
+`;
+
+export const icon_heart = `<svg width="23" height="21" viewBox="0 0 23 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M11.0105 18.6365C15.8558 15.3147 18.3971 12.2928 19.4392 9.76366C21.3962 5.01291 18.0641 2 14.7778 2C13.3756 2 11.9813 2.54898 11.0105 3.74559C10.0395 2.54873 8.64557 2 7.24308 2C3.95706 2 0.624878 5.01333 2.58168 9.76366C3.62386 12.2928 6.16507 15.3147 11.0105 18.6365ZM11.0105 20.6365C10.6154 20.6365 10.2204 20.5197 9.87959 20.2861C5.13987 17.0367 2.06235 13.7529 0.732516 10.5256C-0.373284 7.84119 -0.224294 5.23099 1.15195 3.17563C2.46355 1.21683 4.79753 0 7.24308 0C8.61323 0 9.91515 0.385814 11.0105 1.09276C12.1058 0.385817 13.4077 0 14.7778 0C17.2236 0 19.5576 1.21678 20.8691 3.17549C22.2453 5.23077 22.3943 7.84102 21.2885 10.5254C19.9586 13.7529 16.881 17.0367 12.1413 20.2861C11.8006 20.5197 11.4055 20.6365 11.0105 20.6365Z" fill="black"/>
+</svg>
+`;
+
+export const icon_heart_active = `<svg width="23" height="21" viewBox="0 0 23 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M11.0112 18.6365C15.8566 15.3147 18.3978 12.2928 19.44 9.76366C21.3969 5.01291 18.0649 2 14.7786 2C13.3764 2 11.982 2.54898 11.0112 3.74559C10.0402 2.54873 8.6463 2 7.24381 2C3.95779 2 0.62561 5.01333 2.58241 9.76366C3.62459 12.2928 6.1658 15.3147 11.0112 18.6365ZM11.0112 20.6365C10.6161 20.6365 10.2211 20.5197 9.88032 20.2861C5.1406 17.0367 2.06308 13.7529 0.733249 10.5256C-0.372551 7.84119 -0.223561 5.23099 1.15268 3.17563C2.46428 1.21683 4.79826 0 7.24381 0C8.61396 0 9.91588 0.385814 11.0112 1.09276C12.1065 0.385817 13.4084 0 14.7786 0C17.2243 0 19.5583 1.21678 20.8699 3.17549C22.2461 5.23077 22.395 7.84102 21.2892 10.5254C19.9593 13.7529 16.8818 17.0367 12.1421 20.2861C11.8013 20.5197 11.4062 20.6365 11.0112 20.6365Z" fill="#004CFF"/>
+</svg>
+`;
+
+export const icon_categories = `<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M18 1H4C2.34315 1 1 2.34315 1 4V18C1 19.6569 2.34315 21 4 21H18C19.6569 21 21 19.6569 21 18V4C21 2.34315 19.6569 1 18 1Z" stroke="black" stroke-width="2"/>
+<path d="M5.23999 11H16.778" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M5.23999 6.18896H12.891" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M5.23999 15.811H12.891" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
+
+export const icon_categories_active = `<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M18 1H4C2.34315 1 1 2.34315 1 4V18C1 19.6569 2.34315 21 4 21H18C19.6569 21 21 19.6569 21 18V4C21 2.34315 19.6569 1 18 1Z" stroke="#004CFF" stroke-width="2"/>
+<path d="M5.23999 11H16.778" stroke="#004CFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M5.23999 6.18896H12.891" stroke="#004CFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M5.23999 15.811H12.891" stroke="#004CFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
+
+export const icon_cart = `<svg width="23" height="22" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_2036_2465)">
+<g clip-path="url(#clip1_2036_2465)">
+<path d="M1 19.7999V4.76396L4.15829 1H17.9936L21.1519 4.76395V19.7999C21.1519 20.3601 20.6061 20.9999 19.6905 20.9999H2.46132C1.54575 20.9999 1 20.3601 1 19.7999Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M1.59497 4.3999H20.6606" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M15.3905 8.7998C15.3905 11.2299 13.4587 13.1998 11.0757 13.1998C8.69276 13.1998 6.76099 11.2299 6.76099 8.7998" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </g>
 </g>
 <defs>
-<clipPath id="clip0_2_3444">
-<rect width="21.6982" height="21.8012" fill="white" transform="translate(1.20898 1.08105)"/>
+<clipPath id="clip0_2036_2465">
+<rect width="22.1519" height="21.9999" fill="white"/>
 </clipPath>
-<clipPath id="clip1_2_3444">
-<rect width="21.6982" height="21.8012" fill="white" transform="translate(1.20898 1.08105)"/>
+<clipPath id="clip1_2036_2465">
+<rect width="22.1519" height="21.9999" fill="white"/>
 </clipPath>
 </defs>
 </svg>
 `;
 
-export const icon_arrow_right = `<svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M8.67148 0.294922L7.49182 1.49866L11.2475 5.14597H0.0888672V6.83119H11.2475L7.49182 10.4785L8.67148 11.6822L14.5457 5.98858L8.67148 0.294922Z" fill="white"/>
+export const icon_cart_active = `<svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_2_5697)">
+<g clip-path="url(#clip1_2_5697)">
+<path d="M1.97595 19.7999V4.76396L5.13424 1H18.9695L22.1278 4.76395V19.7999C22.1278 20.3601 21.5821 20.9999 20.6665 20.9999H3.43727C2.5217 20.9999 1.97595 20.3601 1.97595 19.7999Z" stroke="#004CFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M2.57092 4.3999H21.6365" stroke="#004CFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M16.3664 8.7998C16.3664 11.2299 14.4346 13.1998 12.0517 13.1998C9.66871 13.1998 7.73694 11.2299 7.73694 8.7998" stroke="#004CFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+</g>
+<defs>
+<clipPath id="clip0_2_5697">
+<rect width="22.1519" height="21.9999" fill="white" transform="translate(0.975952)"/>
+</clipPath>
+<clipPath id="clip1_2_5697">
+<rect width="22.1519" height="21.9999" fill="white" transform="translate(0.975952)"/>
+</clipPath>
+</defs>
+</svg>
+`;
+
+export const icon_profile = `<svg width="22" height="24" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M14.3457 4.67287C14.3457 6.70133 12.7013 8.34573 10.6729 8.34573C8.6444 8.34573 7 6.70133 7 4.67287C7 2.6444 8.6444 1 10.6729 1C12.7013 1 14.3457 2.6444 14.3457 4.67287Z" stroke="black" stroke-width="2"/>
+<g clip-path="url(#clip0_2036_2470)">
+<path d="M20.921 23.0638V18.6728C20.879 17.3932 20.331 16.1826 19.3974 15.3065C18.4637 14.4304 17.2207 13.9605 15.941 13.9998H5.98C4.7003 13.9605 3.45725 14.4304 2.52362 15.3065C1.58999 16.1826 1.04204 17.3932 1 18.6728V23.0638" stroke="black" stroke-width="2"/>
+</g>
+<defs>
+<clipPath id="clip0_2036_2470">
+<rect width="21.921" height="10.064" fill="white" transform="translate(0 13)"/>
+</clipPath>
+</defs>
+</svg>
+`;
+
+export const icon_profile_active = `<svg width="22" height="24" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M14.3457 4.67287C14.3457 6.70133 12.7013 8.34573 10.6729 8.34573C8.6444 8.34573 7 6.70133 7 4.67287C7 2.6444 8.6444 1 10.6729 1C12.7013 1 14.3457 2.6444 14.3457 4.67287Z" stroke="#004CFF" stroke-width="2"/>
+<g clip-path="url(#clip0_2_5715)">
+<path d="M20.921 23.0638V18.6728C20.879 17.3932 20.331 16.1826 19.3974 15.3065C18.4637 14.4304 17.2207 13.9605 15.941 13.9998H5.98C4.7003 13.9605 3.45725 14.4304 2.52362 15.3065C1.58999 16.1826 1.04204 17.3932 1 18.6728V23.0638" stroke="#004CFF" stroke-width="2"/>
+</g>
+<defs>
+<clipPath id="clip0_2_5715">
+<rect width="21.921" height="10.064" fill="white" transform="translate(0 13)"/>
+</clipPath>
+</defs>
 </svg>
 `;
